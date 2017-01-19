7682d647b6e9d78e2a3f68c5150990ae98c8d1a5
Brian Schaper <briansc@gmail.com>
2017-01-18 23:53:56 -0800

    Added mysql pipeline untested

--- cruisecritic/cruisecritic/pipelines.py
+++ cruisecritic/cruisecritic/pipelines.py
@@ -5,7 +5,28 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sys
+import MySQLdb
+import hashlib
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
 
 class CruisecriticPipeline(object):
+    def __init__(self):
+        self.conn = MySQLdb.connect('bambooiq.ddns.net', 'dbBambooDev', 'B@mboo99', 'dbBambooDev', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+
     def process_item(self, item, spider):
+        try:
+            self.cursor.execute("""INSERT INTO cc (name)
+                            VALUES (%s)""",
+                                (item['name'].encode('utf-8')))
+
+            self.conn.commit()
+
+
+        except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+
         return item
